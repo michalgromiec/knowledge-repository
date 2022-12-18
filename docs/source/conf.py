aafd5a1a1902a06c3cfbc8b21120e08e148d60e7
michalgromiec <michal.gromiec@poczta.onet.pl>
2022-12-18 22:50:14 +0100

    try fix requiremens install

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.duration', 'sphinx.ext.doctest', 'sphinx.ext.autodoc', 'sphinx.ext.autosummary']
+extensions = ['sphinx.ext.duration', 'sphinx.ext.doctest', 'sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'furo']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,7 +24,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster' # furo
+html_theme = 'furo'
 html_static_path = ['_static']
 html_logo = "https://ocdn.eu/pulscms-transforms/1/p8nk9kqTURBXy80NThmZDJiNzY1NjNiMWRmODZmNGJhMGI0OTljZWY4ZS5qcGVnk5UDAwDNAwfNAbSTBc0DFM0BvJMJpmZiNDIwNAbeAAGhMAU/onet-wprowadza-nowe-logo-i-haslo-wiem-z-onet.webp"
 html_theme_options = {"announcement": "<strong>Important <font color='red'>NEWS</font></strong>TESTOWE OSTRZEŻENIE"}
